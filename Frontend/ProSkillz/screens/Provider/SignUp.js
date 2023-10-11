818e4f8006298b950dc220fed2e0f92758ea204f
Deep <kawaledeep9@gmail.com>
2023-10-12 00:59:13 +0530

    Completed Provider Sign Up Screen with Categories Section. (Nesting Scrollview issue ignored!!!)

--- Frontend/ProSkillz/screens/Provider/SignUp.js
+++ Frontend/ProSkillz/screens/Provider/SignUp.js
@@ -1,14 +1,32 @@
-import { View, Text, TouchableOpacity, StyleSheet, ScrollView } from 'react-native'
-import React, { useContext } from 'react'
+import { View, Text, TouchableOpacity, StyleSheet, ScrollView, LogBox } from 'react-native'
+import React, { useContext, useState } from 'react'
 import SignUpP from '../../assets/icons/SignUpP.svg'
 import InputF from '../../components/InputF'
 import ButtonsPS from '../../components/ButtonsPS'
 import { useNavigation } from '@react-navigation/native'
 import AuthContext from '../../context/AuthContext'
+import DropDownPicker from 'react-native-dropdown-picker';
+
+LogBox.ignoreAllLogs();
 
 const SignUp = () => {
 
     const navigation = useNavigation();
+
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(null);
+    const [items, setItems] = useState([
+        { label: 'Laundary', value: 'apple' },
+        { label: 'Cleaning', value: 'Cleaning' },
+        { label: 'MakeUp', value: 'MakeUp' },
+        { label: 'Repairs', value: 'Repairs' },
+        { label: 'Plumbing', value: 'Plumbing' },
+        { label: 'Saloon', value: 'Saloon' },
+        { label: 'Moving', value: 'Moving' },
+        { label: 'Caterers', value: 'Caterers' },
+        { label: 'AirConditioning', value: 'Air Conditioning' },
+        { label: 'Furniture', value: 'Furniture' },
+    ]);
 
     const { updateLoginStateS } = useContext(AuthContext);
 
@@ -34,7 +52,17 @@ const SignUp = () => {
                             <InputF title="Phone" inpM="tel" />
                         </View>
                         <View style={styles.inpF}>
-                            <InputF title="Category" />
+                            <DropDownPicker
+                                style={{ backgroundColor: "transparent" }}
+                                placeholder='Category'
+                                placeholderStyle={{ opacity: 0.55, marginLeft: 7 }}
+                                open={open}
+                                value={value}
+                                items={items}
+                                setOpen={setOpen}
+                                setValue={setValue}
+                                setItems={setItems}
+                            />
                         </View>
                         <View style={styles.inpF}>
                             <InputF title="Email" inpM="email" />
@@ -80,7 +108,8 @@ const styles = StyleSheet.create({
 
     inpF: {
         marginTop: 7,
-        marginBottom: 7
+        marginBottom: 7,
+        position: "relative"
     },
 
     btn: {
