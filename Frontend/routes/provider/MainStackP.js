22254c08af4c80ded0bf87b45edddd1bc7a40452
Deep Kawale <76204769+Deepkk-9@users.noreply.github.com>
2024-03-26 00:01:30 +0530

    Setup router Stack and Tab navigator for Service Provider frontend

--- Frontend/routes/provider/MainStackP.js
+++ Frontend/routes/provider/MainStackP.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from '../../screens/Customer/HomeScreen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import AuthContext from '../../context/AuthContext';
+import TabStackP from './TabStackP';
 
-export default function MainStackP() {
+export default function MainStackU() {
+
+    const { updateLoginStateS, updateAuthState } = useContext(AuthContext);
+
+    useEffect(() => {
+        if (AsyncStorage.getItem("loggedServiceProvider")) {
+            updateAuthState(false);
+            updateLoginStateS(true);
+        }
+    }, [])
 
     const Stack = createNativeStackNavigator();
 
     return (
-        <Stack.Navigator initialRouteName='HomeScreen'>
-            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Navigator initialRouteName='HomeStack'>
+            <Stack.Screen name="HomeStack" component={TabStackP}
+                options={{ headerShown: false }} />
         </Stack.Navigator>
     )
 }
---
+++ Frontend/screens/Provider/Duejobs.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Duejobs = () => {
+    return (
+        <View>
+            <Text>Duejobs</Text>
+        </View>
+    )
+}
+
+export default Duejobs
+
+const styles = StyleSheet.create({})
